c2bf57a363a686fa0cb3a395fbaceb959fbb7608
Alex Cook <alex_cook@Alexs-MacBook-Pro-2.local>
2019-03-10 17:53:54 -0500

    Remade Salamande Script
    It has not more networking inside of it!

---
+++ AnimalArenaAttack/Assets/Scripts/EagleScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EagleScript : MonoBehaviour {
+
+    public Animator eagleAnim;
+    public GameObject feather;
+    public GameObject windGust;
+    public GameObject talonSwipeMelee;
+    public GameObject talonSwipeArc;
+    public float GustUsed;
+    public float TalonUsed;
+
+    public Rigidbody2D featherCloneRB;
+    public float baseAttackDuration = 1f;
+
+    public float featherSpeed = 2f;
+
+	public override void Ability1()
+    {
+        GameObject featherClone = (GameObject)Instantiate(feather, transform.position, transform.rotation);
+
+        Vector2 direction = transform.up;
+        featherClone.GetComponent<Rigidbody2D>().velocity = direction * featherSpeed;
+
+        GameObject.Destroy(featherClone, baseAttackDuration);
+
+    }
+
+    public override void Ability2() 
+    {
+	
+
+	}
+}
