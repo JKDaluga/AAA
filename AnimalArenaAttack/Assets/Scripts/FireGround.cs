139b2c4b331d49acff4e8d13d63acd7cbff57a5b
probopastor <43503592+probopastor@users.noreply.github.com>
2019-04-07 20:17:18 -0500

    BurntSpot Particles
    Particles emit from the burnt spot on the ground after lightning strikes

--- AnimalArenaAttack/Assets/Scripts/FireGround.cs
+++ AnimalArenaAttack/Assets/Scripts/FireGround.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class FireGround : MonoBehaviour {
 
+    public ParticleSystem burntSpot;
+
     public float timer;
     public int damage;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        Instantiate(burntSpot, transform.position, Quaternion.Euler(new Vector3(-90, transform.rotation.y, transform.rotation.z)));
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         timer -= Time.deltaTime;
 		
         if(timer <= 0)
         {
             Destroy(this.gameObject);
+
         }
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
