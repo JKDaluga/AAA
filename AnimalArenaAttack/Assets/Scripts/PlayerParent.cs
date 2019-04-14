289837222b38ac17652d6ce67ee2dd10fc21b25d
Ckersting <48395845+Ckersting@users.noreply.github.com>
2019-04-14 17:33:09 -0500

    Eagle Dash now moves through fireballs instead of colliding.
    Invul in playerparent also sets layer to Dodging, then when you return to being vulnerable, layer set back to player. I-Frames on dodge increased to .25 seconds.

--- AnimalArenaAttack/Assets/Scripts/PlayerParent.cs
+++ AnimalArenaAttack/Assets/Scripts/PlayerParent.cs
@@ -249,7 +249,8 @@ public class PlayerParent : MonoBehaviour
                             rollTime = .2f;
                             rollVector = rb.velocity.normalized;
                             isVulnerable = false;
-                            Invoke("setVulnerability", .2f);
+                            gameObject.layer = 11;
+                            Invoke("setVulnerability", .25f);
                             Instantiate(smokeTrail, transform.position, this.transform.rotation);
                             smokeTrail.Play();
                             Invoke("KillSmoke", 1);
@@ -283,7 +284,6 @@ public class PlayerParent : MonoBehaviour
                             Instantiate(smokeTrail, transform.position, this.transform.rotation);
                             smokeTrail.Play();
                             Invoke("KillSmoke", 1);
-
                         }
                     }
                 }
@@ -413,6 +413,8 @@ public class PlayerParent : MonoBehaviour
     void setVulnerability()
     {
         isVulnerable = true;
+        gameObject.layer = 8;
+
     }
     public void Revive()
     {
