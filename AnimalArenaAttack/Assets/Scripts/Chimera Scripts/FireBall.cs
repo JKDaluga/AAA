91cadadcd0b73371dfb629dfe1f8528da5856c50
Alex Cook <alex_cook@Alexs-MacBook-Pro-2.local>
2019-04-21 18:14:16 -0500

    FireBall Explosion sound
    Makes Sound now but we need a real explosion sound soon to replace homer

--- AnimalArenaAttack/Assets/Scripts/Chimera Scripts/FireBall.cs
+++ AnimalArenaAttack/Assets/Scripts/Chimera Scripts/FireBall.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
+    public AudioClip expl;
+   //public AudioClip explosion;
+
+    public GameplayManager gpm;
     public ParticleSystem fireBurst;
 
     public int damage = 30;
     public float fireBallSpeed;
 
+    public AudioSource src;
+
     // Use this for initialization
     void Start()
     {
+        gpm = FindObjectOfType<GameplayManager>();
         GetComponent<Rigidbody2D>().velocity = -transform.up.normalized * fireBallSpeed;
+        src = gpm.GetComponent< AudioSource > ();
         Destroy(gameObject, 5);
+        
     }
 
     // Update is called once per frame
@@ -26,7 +35,9 @@ public class FireBall : MonoBehaviour
         if (col.gameObject.tag == "Eagle" || col.gameObject.tag == "Salamander")
         {
             GameObject fire = Instantiate(fireBurst.gameObject, transform.position, transform.rotation);
+            src.PlayOneShot(expl);
             fireBurst.Play();
+
             Destroy(fire, .3f);
 
             col.gameObject.GetComponent<PlayerParent>().Damage(damage);
